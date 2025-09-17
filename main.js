821feb4d3b85a270f0aa6f9e5a1858abbbd6ef1d
krichi bc2025-2 <krichi306@gmail.com>
2025-09-17 22:43:05 +0300

    pt 3 upd

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-# placeholder
+const date = "20231001"
+const time_period = "q"
+
+const { URL, URLSearchParams } = require("url");
+const base = new URL("https://bank.gov.ua/NBUStatService/v1/statdirectory/basindbank");
+
+base.search = new URLSearchParams({
+  date: date,
+  period: time_period
+}).toString();
+
+base.search += "&json"
+
+console.log(base.toString());
